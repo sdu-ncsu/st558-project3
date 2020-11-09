fb619ee4d5c34e1bb873c93064da1a48e864f1ed
Shuang Du <zealotsd@gmail.com>
2020-11-09 15:49:08 -0500

    tabs and stuff added

--- ui.R
+++ ui.R
@@ -12,27 +12,73 @@ library(shiny)
 library(caret)
 library(tidyverse)
 library(DT)
+library(plotly)
 
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Deaths in Chicago as Affected by Weather"),
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+    tabsetPanel(
+        tabPanel("Map", fluid = TRUE,
+                 sidebarLayout(
+                     sidebarPanel(selectInput("Country", "Select Country", choices = "", selected = "")),
+                     mainPanel(
+                         h3('This data set comes from the '),
+                     )
+                 )
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
+        tabPanel("plot", fluid = TRUE,
+                 sidebarLayout(
+                     sidebarPanel(sliderInput("year", "Year:", min = 1968, max = 2009, value = 2009, sep='')),
+                     mainPanel(fluidRow(
+                         column(7,  plotlyOutput("")),
+                         column(5, plotlyOutput(""))   
+                     )
+                     )
+                 )
         )
     )
+    
+    # Sidebar with a slider input for number of bins
+    # sidebarLayout(
+    #     sidebarPanel(
+    #         sliderInput("bins",
+    #                     "Number of bins:",
+    #                     min = 1,
+    #                     max = 50,
+    #                     value = 30)
+    #     ),
+    # 
+    #     # Show a plot of the generated distribution
+    #     mainPanel(
+    #         plotOutput("distPlot")
+    #     )
+    # )
 ))
+
+
+ui = fluidPage(
+    tabsetPanel(
+        tabPanel("Map", fluid = TRUE,
+                 sidebarLayout(
+                     sidebarPanel(selectInput("Country", "Select Country", choices = "", selected = "")),
+                     mainPanel(
+                         htmlOutput("Attacks")
+                     )
+                 )
+        ),
+        tabPanel("plot", fluid = TRUE,
+                 sidebarLayout(
+                     sidebarPanel(sliderInput("year", "Year:", min = 1968, max = 2009, value = 2009, sep='')),
+                     mainPanel(fluidRow(
+                         column(7,  plotlyOutput("")),
+                         column(5, plotlyOutput(""))   
+                     )
+                     )
+                 )
+        )
+    )
+)
